911cf3be532bdc6374c7df0499d36e0ba5cc9aaa
KolyshkinSerhii <Sparxxx1986@gmail.com>
2023-01-20 19:03:38 +0200

    predeploy

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/specprommash/' element={<Home />} />
         <Route path='/aboutUs' element={<AboutUs />} />
         <Route path='/services' element={<Services />} />
         <Route path='/contacts' element={<Contacts />} />
